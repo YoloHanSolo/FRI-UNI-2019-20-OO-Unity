9198da1dc22f4b4cc2bff599e6b07a4804cb3d8c
YoloHanSolo <jnpelicon@gmail.com>
2019-06-07 23:15:11 +0200

    FinalPush
    Dokončan 1. projekt

--- ImaginacijaSveta/Assets/MoveCamera.cs
+++ ImaginacijaSveta/Assets/MoveCamera.cs
@@ -9,12 +9,10 @@ public class MoveCamera : MonoBehaviour
     public GameObject[] nodes;
     Transform[] look;
     public float speed;
+    public float slowSpeed;
     public float damping;
     public int size;
     int p = 0;
-	
-	public double timer = 4.0;
-	public double timerMax = 4.0;
 
     void Start()
     {
@@ -30,29 +28,26 @@ public class MoveCamera : MonoBehaviour
 
     void Update()
     {
-		timer += Time.deltaTime;
-		float step;
-		
-		if (timer >= timerMax) {
-			if (p % 2 == 0) {
-				step = speed * Time.deltaTime;
-			} else {
-				step = speed * Time.deltaTime / 2;
-			}
-			maincamera.transform.position = Vector3.MoveTowards(maincamera.transform.position, new Vector3(nodes[p].transform.position.x, nodes[p].transform.position.y, nodes[p].transform.position.z), step);
-			Quaternion rotation = Quaternion.LookRotation(look[p].position - maincamera.transform.position);
-			maincamera.transform.rotation = Quaternion.Slerp(maincamera.transform.rotation, rotation, Time.deltaTime * damping);
 
-			if (Vector3.Distance(maincamera.transform.position, nodes[p].transform.position) < 0.001f)
+        float step;
+        if ( p == 2 || p == 3 || p == 6 || p == 9 || p == 10 || p == 13 || p == 14 || p == 17) {
+            step = slowSpeed * Time.deltaTime;
+        } else {
+            step = speed * Time.deltaTime;
+        }
+		maincamera.transform.position = Vector3.MoveTowards(maincamera.transform.position, new Vector3(nodes[p].transform.position.x, nodes[p].transform.position.y, nodes[p].transform.position.z), step);
+		Quaternion rotation = Quaternion.LookRotation(look[p].position - maincamera.transform.position);
+		maincamera.transform.rotation = Quaternion.Slerp(maincamera.transform.rotation, rotation, Time.deltaTime * damping);
+
+		if (Vector3.Distance(maincamera.transform.position, nodes[p].transform.position) < 0.001f)
+		{
+			p++;
+			if( p >= size)
 			{
-				//timer = 0.0;
-				p++;
-				if( p >= size)
-				{
-					p = 0;
-				}
+				p = 0;
 			}
 		}
+		
     }
 	
 }
